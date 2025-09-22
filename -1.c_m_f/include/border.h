139c189dc50e55107cadb51b11c8b3a9d1cf8a7c
郑阳康 <2126483838@qq.com>
2025-09-22 13:56:41 +0800

    border with own style over!

--- -1.c_m_f/include/border.h
+++ -1.c_m_f/include/border.h
@@ -3,11 +3,12 @@
 #include <string.h>
 
 /*border column 1 & 5*/
-void border_1_5(const char *str, int i);
+void border_1_5(const char *str, int i, const char *ch_border);
 /*border column 2 & 4*/
-void border_2_4(const char *str, int i);
+void border_2_4(const char *str, int i, const char *ch_border);
 /*border column 3*/
-void border_3(const char *str, int i);
-void border_print(const char *str, int i);
+void border_3(const char *str, int i, const char *ch_border);
+void border_print_5_columns(const char *str, int i, const char *ch_border);
+void border_print_3_columns(const char *str, int i, const char *ch_border);
 
 #endif /* BORDER_H_ */
