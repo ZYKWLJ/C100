67f01a3f6defc8d15c19986ee4b4bd0576fc7b09
郑阳康 <2126483838@qq.com>
2025-09-22 12:55:54 +0800

    border create over!

--- -1.c_m_f/src/c_bitcask.c
+++ -1.c_m_f/src/c_bitcask.c
@@ -3,18 +3,30 @@
 
 #define READ_TIME 3
 int count = 0;
+/*i -> border width*/
+void help_border(const char *str, int i)
+{
+    int j = i;
+    while (j-- > 0)
+        printf("%s", j == i-1 || j == 0 ? "#" : " ");
+    printf("\n");
+}
 void border_print(const char *str, int i)
 {
     int j = i;
     while (j-- > 0)
-        printf("#");
+        printf("%s", !j ? "#\n" : "#");
+    help_border(str, i);
+    j = i;
+    printf("%s  %s", "#", str);
+    j -= strlen(str) + 4;
+    while (j-- > 0)
+        printf(" ");
+    printf("#\n");
+    help_border(str, i);
     j = i;
     while (j-- > 0)
-        printf
-        printf("%s", !j || j == i ? "#" :(j-i==strlen(str)?(j)) " ");
-    j = i;
-    while (j-- > 0)
-        printf("#");
+        printf("%s", !j ? "#\n" : "#");
 }
 void search()
 {
@@ -48,8 +60,10 @@ void update()
 void delete()
 {
 }
+
 int main(void)
 {
+#if 0
     while (count < READ_TIME)
     {
         printf("turn %d\t", count);
@@ -65,5 +79,9 @@ int main(void)
         /*test4 start delete data with key*/
         delete();
     }
+#endif
+    border_print("hello", 80);
+    border_print("hello Alice", 80);
+    border_print("hello Jack", 80);
     return 0;
 }
